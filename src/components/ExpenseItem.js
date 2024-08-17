6d211fd3d5d749172c38dd76b78e929d0a2c3fc0
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-17 08:52:08 +0300

    initial commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  function handleTitleChange() {
+    setTitle("Got clicked");
+    console.log(title);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleTitleChange}>Change Title</button>
     </Card>
   );
 }
